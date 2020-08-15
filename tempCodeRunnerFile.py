eb072a058b8e57c1375f597af6d4b8250c42430d
JeffreyWashburn <washburn1197@gmail.com>
2020-08-15 17:16:55 -0400

    Delete temp files and update .gitignore

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-for submission in subreddit.hot(limit=5):
-    print("Title: ", submission.title)
-    print("Text: ", submission.selftext)
-    print("Score: ", submission.score)
-    print("---------------------------------\n")
