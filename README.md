f94c0a112cc89382c9e3d0536651dc65c1186369
JeffreyWashburn <washburn1197@gmail.com>
2020-08-15 17:58:42 -0400

    Add sms notifications via email, update documentation

--- README.md
+++ README.md
@@ -3,8 +3,14 @@
 ### Install dependencies
 * pip install win10toast
 * pip install praw
+### Get your credentials
+I recommend doing what I did with creating a creds.py file to hold all your keys and stuff so that it's secured, otherwise you can hardcode them into the script yourself, just replace the values in the script.
+### Set up your gmail to send texts
+1. Go to your gmail's [security settings](myaccount.google.com/security) and enable two-factor authentication
+1. On the same page, you should now see an app passwords section. Go in there and create a new app and get a password for it. You will use this password in the script.
+1. Look at [this article](https://www.digitaltrends.com/mobile/how-to-send-a-text-from-your-email-account/) to get your "phone number email", use this in place of an email address to instead get sms text messages.
 ### Authenticate with reddits API
-* Paste your client_id, client_secret, and user_agent strings into creds.py (these will then be imported and used to instantate the reddit object in the script)
-* To get these go to your [apps](https://reddit.com/prefs/apps) on the reddit site and add an app
+* Paste your client_id, client_secret, and user_agent strings into creds.py (these will then be imported and used to instantate the reddit object in the script).
+* To get these go to your [apps](https://reddit.com/prefs/apps) on the reddit site and add an app.
 ### Common errors
-* recieved err 401 - This most likely means your credentials are wrong
+* recieved err 401 - This most likely means your credentials are wrong.
--- script.py
+++ script.py
@@ -1,12 +1,13 @@
 # Imports
 import praw
 import re
+import smtplib
+from email.message import EmailMessage
 from win10toast import ToastNotifier
-from sys import exit
 from time import sleep
 
 # bring in my private credentials
-from private.creds import client_id, client_secret, user_agent
+from private.creds import *
 
 # Constants (Enter your search patterns here)
 NOTIFY_IMG = "icons/hardwareswap.ico"
@@ -15,10 +16,28 @@ IS_PAYPAL = re.compile(r"PayPal", re.IGNORECASE)
 IS_USA = re.compile(r"^\[USA")
 
 # Functions
-def notify(post):
+def win_notify(post):
     toaster = ToastNotifier()
     toaster.show_toast(title="Found on r/hardwareswap", msg=post, 
         icon_path=NOTIFY_IMG)
+
+def email_alert(subject, body, to):
+    message = EmailMessage()
+    message.set_content(body)
+    message['subject'] = subject
+    message['to'] = to
+    message['from'] = to
+
+    user = email[0]
+    password = email[1]
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+
+    server.send_message(message)
+
+    server.quit()
 
 def get_newest(sub_reddit):
     return [post.title for post in sub_reddit.new() if not post.
@@ -59,12 +78,13 @@ def main():
     while True:
         sleep(5)
         # check against expressions
-        # add more pass_# checks as necessary
+        # customize and add more checks as necessary
         pass_1 = check(hardwareswap, PATTERN1)
         for i in range(len(pass_1)):
             post = pass_1[i]
             if post not in discovered:
-                notify(post)
+                win_notify(post)
+                email_alert("Found on r/hardwareswap", post, phoneno)
                 sleep(2)
                 discovered.append(post)
 
